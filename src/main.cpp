b16bcc36ac3c1e1b319a9f6dc599e2197789179d
JonyDProgramer <151613163+JonyDProgramer@users.noreply.github.com>
2025-02-17 12:58:34 +0100

    clase1
    estab inentando entender codigo

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,35 @@
 #include <iostream>
 using namespace std;
 
+// defines
 
+#define screenWidth 800
+#define screenHeight 450
+
+// structs
+
+typedef  struct Player {
+	Rectangle rec;
+	Vector2 speed;
+	Color color;
+}Player;
+
+typedef  struct Enemy {
+	Rectangle rec;
+	Vector2 speed;
+	bool active;
+	Color color;
+};
+
+
+struct Shoot {
+	Rectangle rec;
+	Vector2 speed;
+	bool active;
+	Color color;
+}Shoot;
+
+// Basic Functions
 
 void InitGame() {};
 void UpdateGame() {};
@@ -10,13 +38,28 @@ void DrawGame() {};
 void UnloadGame() {};
 void UpdateDrawFrame() {};
 
-struct {
-	int val;
-};
+// Global variables
+
+bool gameOver = false;
+bool pause = false;
+int score = 0;
+bool victory = false;
+
+static Player player = { 0 };
+static Enemy enemy[10] = { 0 };
+
+int shootRate = 0;
+float alpha = 0.0f;
+
+int activeEnemies = 0;
+int enemiesKill = 0;
+bool smooth = false;
+
+// main 
 
 int main() {
 
-	InitWindow(800, 400, "Galaga88");
+	InitWindow(screenWidth, screenHeight, "Galaga88");
 
 	InitGame();
 
@@ -25,9 +68,40 @@ int main() {
 	while (!WindowShouldClose()) { UpdateDrawFrame(); }
 
 	UnloadGame();
-
+	
 	CloseWindow();
 	return 0;
+}
+
+void InitGame() {
+
+	//init game variables
+
+	pause = false;
+	gameOver = false;
+	victory = false;
+	smooth = false;
+	activeEnemies = 10;
+	enemiesKill = 0;
+	score = 0;
+	alpha = 0;
+
+	//Init Player
+
+	player.rec.x = 20;
+	player.rec.y = 50;
+	player.rec.width = 20;
+	player.rec.height = 50;
+	player.speed.x = 5;
+	player.speed.y = 5;
+	player.color = RED;
+	
+	
+	// Init Enemie
+
+	for (int i = 0; i < 10; i++) {
+		
+	}
 }
 
 
