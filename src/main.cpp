e5a8157f3a54904e1163d80707257b4e085beaf5
JonyDProgramer <151613163+JonyDProgramer@users.noreply.github.com>
2025-02-24 12:48:43 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ typedef  struct Player {
 	Color color;
 }Player;
 
-typedef  struct Enemy {
+ struct Enemy {
 	Rectangle rec;
 	Vector2 speed;
 	bool active;
@@ -28,13 +28,13 @@ struct Shoot {
 	Vector2 speed;
 	bool active;
 	Color color;
-}Shoot;
+}shoot[50];
 
 // Basic Functions
 
-void InitGame() {};
+void InitGame();
 void UpdateGame() {};
-void DrawGame() {};
+void DrawGame(){};
 void UnloadGame() {};
 void UpdateDrawFrame() {};
 
@@ -74,36 +74,46 @@ int main() {
 }
 
 
-//void InitGame() {
-//
-//	//init game variables
-//
-//	pause = false;
-//	gameOver = false;
-//	victory = false;
-//	smooth = false;
-//	activeEnemies = 10;
-//	enemiesKill = 0;
-//	score = 0;
-//	alpha = 0;
-//
-//	//Init Player
-//
-//	player.rec.x = 20;
-//	player.rec.y = 50;
-//	player.rec.width = 20;
-//	player.rec.height = 50;
-//	player.speed.x = 5;
-//	player.speed.y = 5;
-//	player.color = RED;
-//	
-//	
-//	// Init Enemie
-//
-//	for (int i = 0; i < 10; i++) {
-//		
-//	}
-//}
+void InitGame() {
+
+	//init game variables
+
+	pause = false;
+	gameOver = false;
+	victory = false;
+	smooth = false;
+	activeEnemies = 10;
+	enemiesKill = 0;
+	score = 0;
+	alpha = 0;
+
+	//Init Player
+
+	player.rec.x = 20;
+	player.rec.y = 50;
+	player.rec.width = 20;
+	player.rec.height = 50;
+	player.speed.x = 5;
+	player.speed.y = 5;
+	player.color = RED;
+	
+	
+	// Init Enemie
+
+	
+	// Initialize shoots
+	for (int i = 0; i < 50; i++)
+	{
+		shoot[i].rec.x = player.rec.x;
+		shoot[i].rec.y = player.rec.y + player.rec.height / 4;
+		shoot[i].rec.width = 10;
+		shoot[i].rec.height = 5;
+		shoot[i].speed.x = 7;
+		shoot[i].speed.y = 0;
+		shoot[i].active = false;
+		shoot[i].color = MAROON;
+	}
+}
 
 
 
