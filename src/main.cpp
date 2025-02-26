20e86a910aeba0c14d56bca6d43e5ce0e7c192b2
JonyDProgramer <151613163+JonyDProgramer@users.noreply.github.com>
2025-02-26 09:54:34 +0100

    prueba_finished
    mas o menos podemos ver como funciona el space invaders de raylib

--- src/main.cpp
+++ src/main.cpp
@@ -100,14 +100,14 @@ void InitGame() {
 	
 	// Init Enemie
 	for (int i = 0; i < 10; i++) {
-		enemy[i].rec.x = 10;
-		enemy[i].rec.y = 10;
+		enemy[i].rec.width = 10;
+		enemy[i].rec.height = 10;
 		enemy[i].rec.x = GetRandomValue(screenWidth,screenWidth + 1000);
 		enemy[i].rec.y = GetRandomValue(0, screenHeight - enemy[i].rec.height);
 		enemy[i].speed.x = 5;
 		enemy[i].speed.y = 5;
 		enemy[i].active = true;
-		enemy[i].color = YELLOW;
+		enemy[i].color = BLACK;
 	}
 
 	
@@ -129,16 +129,106 @@ void InitGame() {
 
 void UpdateGame()
 {
+	if (!gameOver || !victory) {
+		if (IsKeyPressed('P')) { pause = !pause; }
 
+		if (!pause) {
+			
+			//Player movement
+			if (IsKeyDown('W')) { player.rec.y -= player.speed.y; }
+			if (IsKeyDown('A')) { player.rec.x -= player.speed.x; }
+			if (IsKeyDown('S')) { player.rec.y += player.speed.y; }
+			if (IsKeyDown('D')) { player.rec.x += player.speed.x; }
+
+			if (IsKeyDown(KEY_UP)) { player.rec.y -= player.speed.y; }
+			if (IsKeyDown(KEY_LEFT)) { player.rec.x -= player.speed.x; }
+			if (IsKeyDown(KEY_DOWN)) { player.rec.y += player.speed.y; }
+			if (IsKeyDown(KEY_RIGHT)) { player.rec.x += player.speed.x; }
+
+			//Colision With enemy
+			for (int i = 0; i < activeEnemies; i++)
+			{
+				if (CheckCollisionRecs(player.rec, enemy[i].rec)) { gameOver = true; }
+			}
+
+			//Enemy
+			for (int i = 0; i < activeEnemies; i++) {
+				if (enemy[i].active) {
+					enemy[i].rec.x -= enemy[i].speed.x;
+				}
+				if (enemy[i].rec.x < 0)
+				{
+					enemy[i].rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
+					enemy[i].rec.y = GetRandomValue(0, screenHeight - enemy[i].rec.height);
+				}
+			}
+
+			//Walls
+			if (player.rec.x <= 0) { player.rec.x = 0; }
+			if (player.rec.x + player.rec.width >= screenWidth) { player.rec.x = screenWidth - player.rec.width; }
+			if (player.rec.y <= 0) { player.rec.y = 0; }
+			if (player.rec.y + player.rec.height >= screenHeight) { player.rec.y = screenHeight - player.rec.height; }
+
+			//Shoots
+			if (IsKeyDown(KEY_SPACE)) {
+
+				shootRate += 5;
+
+				for (int i = 0; i < 50; i++) {
+					if (!shoot[i].active && shootRate % 20 == 0) {
+						shoot[i].rec.x = player.rec.x;
+						shoot[i].rec.y = player.rec.y + player.rec.height / 4;
+						shoot[i].active = true;
+						break;
+					}
+				}
+
+			}
+
+			for (int i = 0; i < 50; i++) {
+				if (shoot[i].active) {
+					shoot[i].rec.x += shoot[i].speed.x;
+
+					//collision with enemy
+					for (int j = 0; j < activeEnemies; j++) {
+						if (enemy[j].active) {
+							if (CheckCollisionRecs(shoot[i].rec, enemy[j].rec)) {
+								shoot[i].active = false;
+								enemy[j].rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
+								enemy[j].rec.y = GetRandomValue(0, screenHeight - enemy[j].rec.height);
+								enemiesKill++;
+								score += 100;
+							}
+						}
+
+						if(shoot[i].rec.x + shoot[i].rec.width >= screenWidth){
+							shoot[i].active = false;
+							shootRate = 0;
+						}
+					}
+				}
+			}
+
+			if (score >= 5000) { victory = true; }
+		}
+	}
+	else {
+		if (IsKeyPressed(KEY_ENTER)) {
+			InitGame();
+			gameOver = false;
+			victory = false;
+		}
+	}
 }
 
 void DrawGame()
 {
 	BeginDrawing();
-	ClearBackground(SKYBLUE);
+	ClearBackground(WHITE);
 
 	if (!gameOver) {
 		DrawRectangleRec(player.rec, player.color);
+		
 
 		for (int i = 0; i < activeEnemies; i++) {
 			if (enemy[i].active) DrawRectangleRec(enemy[i].rec, enemy[i].color);
