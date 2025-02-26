054b8d05e5b565349cbd0268b59ae31104d4d51e
JonyDProgramer <151613163+JonyDProgramer@users.noreply.github.com>
2025-02-26 09:02:16 +0100

    end Drawing SpaceInvaders

--- src/main.cpp
+++ src/main.cpp
@@ -33,10 +33,10 @@ struct Shoot {
 // Basic Functions
 
 void InitGame();
-void UpdateGame() {};
-void DrawGame(){};
-void UnloadGame() {};
-void UpdateDrawFrame() {};
+void UpdateGame();
+void DrawGame();
+void UnloadGame();
+void UpdateDrawFrame();
 
 // Global variables
 
@@ -99,6 +99,16 @@ void InitGame() {
 	
 	
 	// Init Enemie
+	for (int i = 0; i < 10; i++) {
+		enemy[i].rec.x = 10;
+		enemy[i].rec.y = 10;
+		enemy[i].rec.x = GetRandomValue(screenWidth,screenWidth + 1000);
+		enemy[i].rec.y = GetRandomValue(0, screenHeight - enemy[i].rec.height);
+		enemy[i].speed.x = 5;
+		enemy[i].speed.y = 5;
+		enemy[i].active = true;
+		enemy[i].color = YELLOW;
+	}
 
 	
 	// Initialize shoots
@@ -111,9 +121,47 @@ void InitGame() {
 		shoot[i].speed.x = 7;
 		shoot[i].speed.y = 0;
 		shoot[i].active = false;
-		shoot[i].color = MAROON;
+		shoot[i].color = BLUE;
 	}
 }
 
+
+
+void UpdateGame()
+{
+
+}
+
+void DrawGame()
+{
+	BeginDrawing();
+	ClearBackground(SKYBLUE);
+
+	if (!gameOver) {
+		DrawRectangleRec(player.rec, player.color);
+
+		for (int i = 0; i < activeEnemies; i++) {
+			if (enemy[i].active) DrawRectangleRec(enemy[i].rec, enemy[i].color);
+		}
+		for (int i = 0; i < 50; i++)
+		{
+			if (shoot[i].active) DrawRectangleRec(shoot[i].rec, shoot[i].color);
+		}
+		DrawText(TextFormat("%04i", score), 20, 20, 40, BLACK);
+
+		if (victory) DrawText("YOU WIN", screenWidth / 2 - MeasureText("YOU WIN", 40) / 2, screenHeight / 2 - 40, 40, BLACK);
+		if (pause) DrawText("GAME PAUSED", screenWidth / 2 - MeasureText("GAME PAUSED", 40) / 2, screenHeight / 2 - 40, 40, GRAY);
+	}
+	else DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth() / 2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20) / 2, GetScreenHeight() / 2 - 50, 20, GRAY);
+
+	EndDrawing();
+}
+
+void UnloadGame() {}
+void UpdateDrawFrame()
+{
+	UpdateGame();
+	DrawGame();
+}
 
 
