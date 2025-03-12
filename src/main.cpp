ac45e75c4ef2270966431f20016b287db61d9551
Xmacias365 <159776765+xavimacias365@users.noreply.github.com>
2025-03-12 11:11:33 +0100

    Implemented enemy sprite (II)

--- src/main.cpp
+++ src/main.cpp
@@ -62,6 +62,7 @@ Texture2D background;
 Texture2D player_sprite;
 Texture2D shoot_sprite;
 Texture2D level_sprite;
+Texture2D zako_enemy_sprite;
 
 
 
@@ -145,6 +146,7 @@ void InitGame() {
 	player_sprite = LoadTexture("Textures/entities/player/Fighter.png");
 	shoot_sprite = LoadTexture("Textures/entities/enemies/projectiles/disparoPlayer.png");
 	level_sprite = LoadTexture("Textures/items/stage_indicator.png");
+	zako_enemy_sprite = LoadTexture("Textures/entities/enemies/zako_dim1.png");
 }
 void UpdateGame() {
 	if (!gameOver && !victory) {
@@ -301,7 +303,8 @@ void DrawGame() {
 
 		for (int i = 0; i < activeEnemies; i++) {
 			if (enemy[i].active) {
-				DrawRectangleRec(enemy[i].rec, enemy[i].color);
+//				DrawRectangleRec(enemy[i].rec, enemy[i].color);
+				DrawTextureEx(zako_enemy_sprite, { enemy[i].rec.x, enemy[i].rec.y }, 0.0f, (enemy[i].rec.width / zako_enemy_sprite.width, enemy[i].rec.height / ((zako_enemy_sprite.height) / 2)), WHITE);
 			}
 			
 		}
